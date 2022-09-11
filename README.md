a6a026ac920cb33bafca6426f646ca4595226e5a
Tadaomi Kasuya <dtl.reco@gmail.com>
2022-09-11 20:23:37 +0700

    gulpに変更

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# 20220806sengokuya
+# gulp-test
+## How to Use
+```
+$ git clone https://github.com/Ichikun0000/gulp-test.git
+```
+```
+$ cd _gulp
+```
+```
+$ npm i
+```
+```
+$ npx gulp
+```
---
+++ _assets/js/swiper.js
@@ -0,0 +1,12 @@
+'use strict'
+{
+
+  const swiper = new Swiper(".swiper", {
+    spaceBetween: 10,
+    slidesPerView: 'auto',
+    loop: true,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+
+}
