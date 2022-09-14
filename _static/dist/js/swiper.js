434bbcf236e56c02540cc71b62c2cdaf845458fe
Tadaomi Kasuya <dtl.reco@gmail.com>
2022-09-14 15:36:04 +0700

    スライダー修正

--- _static/dist/js/swiper.js
+++ _static/dist/js/swiper.js
@@ -2,10 +2,17 @@
 {
 
   const swiper = new Swiper(".swiper", {
-    spaceBetween: 10,
-    slidesPerView: 'auto',
-    loop: true,
-    watchSlidesProgress: true,
+    // spaceBetween: 10,
+  centeredSlides: true,
+	loop: true,
+	slidesPerView: 1.25,
+	spaceBetween: 0,
+    breakpoints: {
+      600: {
+        slidesPerView: 1,
+        slidesPerView: "auto"
+      }
+    },
   });
 
 }
